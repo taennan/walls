0684c65757433f3278dbbf523e7ae3f27c254f8b
Taennan Rickman <Taennan@imac.local>
2020-08-23 17:22:01 +1200

    Initial Commit

---
+++ Walls/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Walls
+//
+//  Created by Taennan Rickman on 23/8/20.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
